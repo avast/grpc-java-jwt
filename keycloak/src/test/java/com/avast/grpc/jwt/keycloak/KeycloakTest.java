4f957d76076a481676cc9a79aa45ecae5f2b9984
Michal Augustyn <augustyn.michal@gmail.com>
2019-04-21 09:49:15 +0200

    one more end-to-end test

--- keycloak/src/test/java/com/avast/grpc/jwt/keycloak/KeycloakTest.java
+++ keycloak/src/test/java/com/avast/grpc/jwt/keycloak/KeycloakTest.java
@@ -1,6 +1,7 @@
 package com.avast.grpc.jwt.keycloak;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import com.avast.grpc.jwt.keycloak.client.KeycloakJwtCallCredentials;
 import com.avast.grpc.jwt.keycloak.server.KeycloakJwtServerInterceptor;
@@ -8,29 +9,29 @@ import com.avast.grpc.jwt.test.TestServiceGrpc;
 import com.avast.grpc.jwt.test.TestServices;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import io.grpc.ManagedChannel;
-import io.grpc.Server;
-import io.grpc.ServerInterceptors;
+import io.grpc.*;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
 import java.io.IOException;
 import org.junit.Test;
 
 public class KeycloakTest {
+  Config config = ConfigFactory.load().getConfig("testKeycloak");
+  String channelName = InProcessServerBuilder.generateName();
+
+  ManagedChannel clientChannel =
+          InProcessChannelBuilder.forName(channelName).usePlaintext().build();
+
+  KeycloakJwtServerInterceptor serverInterceptor =
+          KeycloakJwtServerInterceptor.fromConfig(config);
+  TestServiceImpl service = new TestServiceImpl(serverInterceptor.AccessTokenContextKey);
+
   @Test
   public void endToEndTest() throws IOException {
-    Config config = ConfigFactory.load().getConfig("testKeycloak");
-    String channelName = InProcessServerBuilder.generateName();
-    ManagedChannel clientChannel =
-        InProcessChannelBuilder.forName(channelName).usePlaintext().build();
-
     TestServiceGrpc.TestServiceBlockingStub client =
         TestServiceGrpc.newBlockingStub(clientChannel)
             .withCallCredentials(KeycloakJwtCallCredentials.fromConfig(config));
 
-    KeycloakJwtServerInterceptor serverInterceptor =
-        KeycloakJwtServerInterceptor.fromConfig(config);
-    TestServiceImpl service = new TestServiceImpl(serverInterceptor.AccessTokenContextKey);
     Server server =
         InProcessServerBuilder.forName(channelName)
             .addService(ServerInterceptors.intercept(service, serverInterceptor))
@@ -42,5 +43,24 @@ public class KeycloakTest {
 
     assertEquals(sum.getSum(), 3);
     assertEquals(service.lastAccessToken.getType(), "Bearer");
+  }
+
+  @Test
+  public void rejectsRequestWithoutHeader() throws IOException {
+    TestServiceGrpc.TestServiceBlockingStub client = TestServiceGrpc.newBlockingStub(clientChannel);
+
+    Server server =
+            InProcessServerBuilder.forName(channelName)
+                    .addService(ServerInterceptors.intercept(service, serverInterceptor))
+                    .build()
+                    .start();
+
+    try {
+      TestServices.AddResponse sum =
+              client.add(TestServices.AddParams.newBuilder().setA(1).setB(2).build());
+    } catch (StatusRuntimeException e) {
+      assertFalse(e.getStatus().isOk());
+      assertEquals(Status.UNAUTHENTICATED.getCode(), e.getStatus().getCode());
+    }
   }
 }
